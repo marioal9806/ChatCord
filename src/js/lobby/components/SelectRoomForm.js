9ef8bbfca0381de31274f7c9fb296d1c1d8c4928
Mario Ortega <marioal9806@hotmail.com>
2021-01-09 15:46:12 -0600

    Modify paragraph content in SelectRoomForm

--- src/js/lobby/components/SelectRoomForm.js
+++ src/js/lobby/components/SelectRoomForm.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 function SelectRoomForm(props) {
   return (
     <Fragment>
-      <p>Please select a conversation you would like to join:</p>
+      <p>Please select the conversation you would like to join:</p>
       <select name="room" value={props.selectedRoom.id} onChange={props.handleChange}>
         {props.rooms.map(room => {
           return <option key={room.id} value={room.id}>{room.name}</option>
