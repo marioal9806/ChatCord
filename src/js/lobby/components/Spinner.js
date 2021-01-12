e18061a84ca5b803d9e5de79defd69bf7be0f804
Mario Ortega <marioal9806@hotmail.com>
2021-01-12 11:33:40 -0600

    Implement spinner for lobby page

---
+++ src/js/lobby/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className="spinner-div">
+      <p>Loading...</p>
+      <i className="fas fa-spinner spinner"></i>
+    </div>
+  )
+}
+
+export default Spinner
