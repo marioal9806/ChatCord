0cc48247f3142892bbfdef74f2c0aabaef65d3df
Mario Ortega <marioal9806@hotmail.com>
2021-01-19 14:02:11 -0600

    Scroll down automatically on new message

--- src/js/room/components/MessageBox.js
+++ src/js/room/components/MessageBox.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import Message from './Message'
 
 function MessageBox(props) {
+  const messageBoxNode = useRef(null)
+
+  useEffect(() => {
+    messageBoxNode.current.scrollTop = messageBoxNode.current.scrollHeight
+  }, [props.messages])
+
   return (
-    <section className='message-box'>
+    <section className='message-box' ref={messageBoxNode}>
       <ul>
         {props.messages.map((message, index) => {
           return <Message key={index} message={message}/>
